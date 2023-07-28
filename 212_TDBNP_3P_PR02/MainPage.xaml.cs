f07052afa271b3328608c15ab02fab6c1f7b6d96
danielisaisal <114195823+danielisaisal@users.noreply.github.com>
2023-07-28 10:09:23 -0600

    Initial commit

---
+++ 212_TDBNP_3P_PR02/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Playback;
+using Windows.Media.Core;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _212_TDBNP_3P_PR02
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaPlayer player = new MediaPlayer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private async void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            sbInteraccion1.Begin();
+            sbInteraccion2.Begin();
+            sbInteraccion3.Begin();
+            sbInteraccion4.Begin();
+            sbInteraccion5.Begin();
+            sbInteracion6.Begin();
+            sbInteracion7.Begin();
+            sbDormido.Begin();
+            sbWalking.Begin();
+            sbComer.Begin();
+
+            Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+            Windows.Storage.StorageFile file = await folder.GetFileAsync("UnderTale_Demo_Flowey.mp3");
+
+            player.AutoPlay = false;
+            player.Source = MediaSource.CreateFromStorageFile(file);
+            player.Play();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Animacion_con_Blend_for_visual_studio
+ Animacion hecha con blend for visual studio
